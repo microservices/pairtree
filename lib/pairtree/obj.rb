9888e0bb56cac49d9e5c3ea8df8dd6b93b2edfa4
Michael Klein <mbklein@gmail.com>
2011-06-10 17:46:39 -0500

    Remove the redundant Pairtree::Obj#file method

--- lib/pairtree/obj.rb
+++ lib/pairtree/obj.rb
@@ -1,8 +1,6 @@
 module Pairtree  
   class Obj < ::Dir
     
-    # Alias certain class methods from File to instance methods of Pairtree::Obj
-    # Methods that require a filename, 0..n additional args, and maybe a block
     FILE_METHODS = [:atime, :open, :read, :file?, :directory?, :exist?, :exists?, :file?, :ftype, :lstat, 
       :mtime, :readable?, :size, :stat, :truncate, :writable?, :zero?]
     FILE_METHODS.each do |file_method|
@@ -11,12 +9,11 @@ module Pairtree
       end
     end
 
-    # Methods that require multiple filenames
     def delete *args
       File.delete(*(prepend_filenames(args)))
     end
     alias_method :unlink, :delete
-    
+
     def link *args
       File.link(*(prepend_filenames(args)))
     end
@@ -25,17 +22,10 @@ module Pairtree
       File.rename(*(prepend_filenames(args)))
     end
     
-    # Methods that require non-filename args, followed by filenames
     def utime atime, mtime, *args
       File.utime(atime, mtime, *(prepend_filenames(args)))
     end
     
-    # Alias File.open to Pairtree::Obj#file
-    def file fname, *args, &block
-      File.open(File.join(self.path, fname), *args, &block)
-    end
-    
-    # Override Dir#entries and Dir#each to remove the dotfiles
     def entries
       super - ['.','..']
     end
@@ -44,7 +34,6 @@ module Pairtree
       super { |entry| yield(entry) unless entry =~ /^\.{1,2}$/ }
     end
     
-    # Basic globbing
     def glob(string, flags = 0)
       entries.select { |entry| File.fnmatch(string, entry, flags) }
     end
